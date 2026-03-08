d6064120916ae2a167d61a1289386160a741c2bb
Avto Bagaturia <Avtobagaturia21@gmail.com>
2026-03-08 21:33:02 +0400

    First changes

--- GuessNumberGame.java
+++ GuessNumberGame.java
@@ -18,5 +18,6 @@ public class  GuessNumberGame {
      public static void main(String[] args) {
         System.out.println("Welcome to Number  Guessing Game!");
         System.out.println("Guess a number between 1 and 100");
+        System.out.println("Enter your guess number: " );
      }
 }
