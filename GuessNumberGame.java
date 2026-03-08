83dc5ab27e43e34f6e59091f03708fb7d6398f99
Avto Bagaturia <Avtobagaturia21@gmail.com>
2026-03-08 21:21:56 +0400

    First changes

--- GuessNumberGame.java
+++ GuessNumberGame.java
@@ -1,4 +1,13 @@
+import java.util.Random;
+
+
+
 public class  GuessNumberGame {
+
+     Random random = new Random();
+     int min = 1;
+     int max = 100;
+     int randomNumber = random.nextInt(max - min + 1) + min; 
      public static void main(String[] args) {
         System.out.println("Welcome to Number  Guessing Game!");
         System.out.println("Guess a number between 1 and 100");
