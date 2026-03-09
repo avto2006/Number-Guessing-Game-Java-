bb611a27ff13ecc0a6f70c403dd1aa0ad1146031
Avto Bagaturia <Avtobagaturia21@gmail.com>
2026-03-09 20:48:36 +0400

    First changes

--- GuessNumberGame.java
+++ GuessNumberGame.java
@@ -6,22 +6,42 @@ public class  GuessNumberGame {
 
 
      // Random number logic
-     Random random = new Random();
-     int min = 1;
-     int max = 100;
-     int randomNumber = random.nextInt(max - min + 1) + min; 
+   
 
 
-     // Scanner logic
-      Scanner scanner = new Scanner(System.in);
+    
+     
       
 
      public static void main(String[] args) {
+
+          Random random = new Random();
+          int min = 1;
+          int max = 100;
+          int randomNumber = random.nextInt(max - min + 1) + min; 
+
         System.out.println("Welcome to Number  Guessing Game!");
         System.out.println("Guess a number between 1 and 100");
         
-        int userGuess = scanner.nextInt();
-        System.out.println("Enter your guess number: " + userGuess);
+         // Scanner logic
+        Scanner scanner = new Scanner(System.in);
+        
+        int userGuess = 0;
+
+        while (userGuess != randomNumber) {
+            System.out.println("Enter your guess number: " + userGuess);
+            userGuess = scanner.nextInt();
+
+            if (userGuess < randomNumber) {
+                System.out.println("Too low");
+            } else if(userGuess > randomNumber) {
+                System.out.println("Too high");
+            } else {
+                System.out.println("Correct!");
+            }
+        }
+       
+        
 
         scanner.close();
      }
